0b08ed27d46ba9947a00536ab597dc7345c80eb0
coppa-code <84047011+coppa-code@users.noreply.github.com>
2026-03-27 13:10:24 -0300

    Remove Firebase configuration from firebase-config.js
    Removed firebase configuration details from the file.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,1 @@
-// firebase-config.js
-export const firebaseConfig = {
-    apiKey: "AIzaSyCneLbc1ghgAKlUSIMVs7WYNKC_EjYwW7M",
-    authDomain: "banco-de-horas-57c35.firebaseapp.com",
-    databaseURL: "https://banco-de-horas-57c35-default-rtdb.firebaseio.com",
-    projectId: "banco-de-horas-57c35",
-    storageBucket: "banco-de-horas-57c35.firebasestorage.app",
-    messagingSenderId: "172937844198",
-    appId: "1:172937844198:web:1737f0bc529b7fd458b227"
-};
+
